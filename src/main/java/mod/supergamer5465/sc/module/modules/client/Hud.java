a91ef5e993437ef1e5f1c7d4397bcd6f66fe9260
Supergamer5465 <64598162+Supergamer5465@users.noreply.github.com>
2021-05-27 22:13:39 -0500

    toggleable hud module

---
+++ src/main/java/mod/supergamer5465/sc/module/modules/client/Hud.java
@@ -0,0 +1,10 @@
+package mod.supergamer5465.sc.module.modules.client;
+
+import mod.supergamer5465.sc.module.Category;
+import mod.supergamer5465.sc.module.Module;
+
+public class Hud extends Module {
+	public Hud() {
+		super("Hud", "In-Game Overlay", Category.CLIENT);
+	}
+}
