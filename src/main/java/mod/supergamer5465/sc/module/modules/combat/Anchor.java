352820ec6342c6c6da28a7cb0a93bede100a349f
Destruc69 <85436971+Destruc69@users.noreply.github.com>
2021-10-13 12:36:51 +1100

    Updated Anchor

--- src/main/java/mod/supergamer5465/sc/module/modules/combat/Anchor.java
+++ src/main/java/mod/supergamer5465/sc/module/modules/combat/Anchor.java
@@ -3,6 +3,7 @@ package mod.supergamer5465.sc.module.modules.combat;
 import mod.supergamer5465.sc.event.events.ScUpdateEvent;
 import mod.supergamer5465.sc.module.Category;
 import mod.supergamer5465.sc.module.Module;
+import mod.supergamer5465.sc.setting.settings.BooleanSetting;
 import mod.supergamer5465.sc.setting.settings.FloatSetting;
 import net.minecraft.client.gui.inventory.GuiInventory;
 import net.minecraft.init.Items;
@@ -13,6 +14,7 @@ public class Anchor extends Module {
 
 	FloatSetting downSpeed = new FloatSetting("DownSpeed", this, 0.15f);
 	FloatSetting fallHeight = new FloatSetting("FallHeight", this, 2.0f);
+	BooleanSetting stopMovement = new BooleanSetting("StopMovement", this, true);
 
 	public Anchor() {
 		super("Anchor", "Get in to holes quicker", Category.COMBAT);
@@ -23,8 +25,11 @@ public class Anchor extends Module {
 
 	@SubscribeEvent
 	public void ScUpdateEvent(ScUpdateEvent event) {
-		if (event.getPlayer().fallDistance > fallHeight.getValue())
+		if (event.getPlayer().fallDistance > fallHeight.getValue()) {
 			mc.player.motionY = downSpeed.getValue();
+		}
 
+		if (stopMovement.isEnabled() && event.getPlayer().fallDistance > fallHeight.getValue())
+			mc.player.setSprinting(false);
 	}
 }
