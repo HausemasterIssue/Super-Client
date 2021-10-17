dd7f38aff373a2fdec891cb6804e679cf63fbd3b
Supergamer5465 <64598162+Supergamer5465@users.noreply.github.com>
2021-10-16 19:36:46 -0500

    updated fakeplayer

--- src/main/java/mod/imphack/util/Reference.java
+++ src/main/java/mod/imphack/util/Reference.java
@@ -1,7 +1,7 @@
 package mod.imphack.util;
 
 public class Reference {
-	public static final String MODID = "ImpHack";
+	public static final String MODID = "imphack";
 	public static final String NAME = "ImpHack Revised";
 	public static final String VERSION = "0.3.0";
 	public static final String ACCEPTVERSIONS = "[1.12.2]";
