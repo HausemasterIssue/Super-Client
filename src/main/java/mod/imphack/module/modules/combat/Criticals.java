44b778e53745a036b0edb0a3e8d49eb28c054cca
Supergamer5465 <64598162+Supergamer5465@users.noreply.github.com>
2021-10-16 21:04:06 -0500

    fix NPE

--- src/main/java/mod/imphack/module/modules/combat/Criticals.java
+++ src/main/java/mod/imphack/module/modules/combat/Criticals.java
@@ -1,11 +1,11 @@
 package mod.imphack.module.modules.combat;
 
+import me.zero.alpine.listener.EventHandler;
+import me.zero.alpine.listener.Listener;
 import mod.imphack.event.events.ImpHackEventPacket;
 import mod.imphack.module.Category;
 import mod.imphack.module.Module;
 import mod.imphack.setting.settings.ModeSetting;
-import me.zero.alpine.listener.EventHandler;
-import me.zero.alpine.listener.Listener;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.network.play.client.CPacketPlayer;
@@ -16,35 +16,43 @@ public class Criticals extends Module {
 
 	public Criticals() {
 		super("Criticals", "Makes normal hits critical hits", Category.COMBAT);
-		
+
 		this.addSetting(mode);
 	}
-	
+
 	ModeSetting mode = new ModeSetting("Mode", this, "NCPStrict", new String[] { "NCPStrict", "Packet", "Jump" });
-	
+
 	CPacketUseEntity packet;
 	final Minecraft mc = Minecraft.getMinecraft();
-	
+
 	@EventHandler
-    private final Listener<ImpHackEventPacket.SendPacket> listener = new Listener<>(event -> {
-        if (event.get_packet() instanceof CPacketUseEntity) {
-            if (((CPacketUseEntity) event.get_packet()).getAction() == CPacketUseEntity.Action.ATTACK && packet.getEntityFromWorld((World)this.mc.world) instanceof EntityLivingBase &&
-            		mc.player.onGround && !mc.player.isInLava() && !mc.player.isInWater()) {
-            	if(mode.getMode().equalsIgnoreCase("NCPStrict")) {
-            		mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.062602401692772D, mc.player.posZ, false));
-                    mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.0726023996066094D, mc.player.posZ, false));
-                    mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY, mc.player.posZ, false));
-            	} else if (mode.getMode().equalsIgnoreCase("Packet")) {
-            		mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.2, mc.player.posZ, false));
-            		mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY, mc.player.posZ, false));
-            	} else if (mode.getMode().equalsIgnoreCase("Jump")) {
-            		mc.player.jump();
-                    mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY - 0.05, mc.player.posZ, false));
-                    mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY, mc.player.posZ, false));
-            	}
-            }
-        	}
-        
-    	}
-    );
+	private final Listener<ImpHackEventPacket.SendPacket> listener = new Listener<>(event -> {
+		if (event.get_packet() instanceof CPacketUseEntity) {
+			if (mc.player != null
+					&& ((CPacketUseEntity) event.get_packet()).getAction() == CPacketUseEntity.Action.ATTACK
+					&& packet.getEntityFromWorld((World) this.mc.world) instanceof EntityLivingBase
+					&& mc.player.onGround && !mc.player.isInLava() && !mc.player.isInWater()) {
+				if (mode.getMode().equalsIgnoreCase("NCPStrict")) {
+					mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX,
+							mc.player.posY + 0.062602401692772D, mc.player.posZ, false));
+					mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX,
+							mc.player.posY + 0.0726023996066094D, mc.player.posZ, false));
+					mc.player.connection.sendPacket(
+							new CPacketPlayer.Position(mc.player.posX, mc.player.posY, mc.player.posZ, false));
+				} else if (mode.getMode().equalsIgnoreCase("Packet")) {
+					mc.player.connection.sendPacket(
+							new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.2, mc.player.posZ, false));
+					mc.player.connection.sendPacket(
+							new CPacketPlayer.Position(mc.player.posX, mc.player.posY, mc.player.posZ, false));
+				} else if (mode.getMode().equalsIgnoreCase("Jump")) {
+					mc.player.jump();
+					mc.player.connection.sendPacket(
+							new CPacketPlayer.Position(mc.player.posX, mc.player.posY - 0.05, mc.player.posZ, false));
+					mc.player.connection.sendPacket(
+							new CPacketPlayer.Position(mc.player.posX, mc.player.posY, mc.player.posZ, false));
+				}
+			}
+		}
+
+	});
 }
