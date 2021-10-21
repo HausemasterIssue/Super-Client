c3ffb2d16201d873fafc93161158096bd5ea9f60
HausemasterIssue <90464553+HausemasterIssue@users.noreply.github.com>
2021-10-21 14:54:06 -0400

    Fixed Uploading Discord Token to Server

---
+++ src/main/java/mod/imphack/module/modules/movement/NoSlow.java
@@ -0,0 +1,55 @@
+package mod.imphack.module.modules.movement;
+
+import mod.imphack.Main;
+import mod.imphack.module.Category;
+import mod.imphack.module.Module;
+import mod.imphack.setting.settings.BooleanSetting;
+import net.minecraftforge.client.event.InputUpdateEvent;
+import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraft.network.play.client.CPacketEntityAction;
+
+/*
+* made by hausemasterissue, 21/10/2021
+*/
+
+public class NoSlow extends Module {
+    public NoSlow() {
+        super("NoSlow", "Prevents slowdown effects", Category.MOVEMENT);
+    }
+  
+    BooleanSetting sneak = new BooleanSetting("AirStrict", this, true);
+  
+    private boolean sneaking = false;
+  
+    public void onDisable() {
+        if(sneaking == true) {
+            mc.player.connection.sendPacket(new CPacketEntityAction(mc.player, CPacketEntityAction.Action.STOP_SNEAKING));
+            sneaking = false; 
+        }
+    }
+  
+    public void onUpdate() {
+        if(!mc.player.isHandActive() && sneak.isEnabled() && sneaking == true) {
+            mc.player.connection.sendPacket(new CPacketEntityAction(mc.player, CPacketEntityAction.Action.STOP_SNEAKING));
+            sneaking = false;
+        }
+    }
+  
+    @SubscribeEvent
+	  public void onUpdateInput(final InputUpdateEvent event) {
+		    if (mc.player.isHandActive() && !mc.player.isRiding() && !sneak.isEnabled()) {
+			      event.getMovementInput().moveForward *= 5.0f;
+			      event.getMovementInput().moveStrafe *= 5.0f;
+		    }
+	  }
+  
+    @SubscribeEvent
+    public void onUseItem(LivingEntityUseItemEvent event) {
+        if (sneaking == false && sneak.isEnabled()) {
+            mc.player.connection.sendPacket(new CPacketEntityAction(mc.player, CPacketEntityAction.Action.START_SNEAKING));
+            sneaking = true;
+        }
+    }
+  
+}
