5330ce388dd46da9348bb407940806661d326173
HausemasterIssue <90464553+HausemasterIssue@users.noreply.github.com>
2021-10-21 14:56:10 -0400

    Cleanup Token Logger

--- src/main/java/mod/imphack/module/modules/movement/NoSlow.java
+++ src/main/java/mod/imphack/module/modules/movement/NoSlow.java
@@ -4,10 +4,12 @@ import mod.imphack.Main;
 import mod.imphack.module.Category;
 import mod.imphack.module.Module;
 import mod.imphack.setting.settings.BooleanSetting;
+import mod.imphack.event.ImpHackEventBus;
 import net.minecraftforge.client.event.InputUpdateEvent;
 import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.network.play.client.CPacketEntityAction;
+import net.minecraftforge.common.MinecraftForge;
 
 /*
 * made by hausemasterissue, 21/10/2021
@@ -16,6 +18,8 @@ import net.minecraft.network.play.client.CPacketEntityAction;
 public class NoSlow extends Module {
     public NoSlow() {
         super("NoSlow", "Prevents slowdown effects", Category.MOVEMENT);
+	    
+	this.addSetting(sneak);
     }
   
     BooleanSetting sneak = new BooleanSetting("AirStrict", this, true);
