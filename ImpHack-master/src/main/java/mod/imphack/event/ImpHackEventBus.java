5f60c76813543363e27b368c96ea438d6768ba9b
SaberAspect <74943836+SaberAspect@users.noreply.github.com>
2021-10-28 19:55:45 -0400

    Delete ImpHack-master directory

--- ImpHack-master/src/main/java/mod/imphack/event/ImpHackEventBus.java
+++
@@ -1,8 +0,0 @@
-package mod.imphack.event;
-
-import me.zero.alpine.bus.EventBus;
-import me.zero.alpine.bus.EventManager;
-
-public class ImpHackEventBus {
-	public static final EventBus EVENT_BUS = new EventManager();
-}
